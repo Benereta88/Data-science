78db300660e507c51a5e7b11e3400002401d21af
Benereta88 <beneretahoxha8@gmail.com>
2024-12-17 10:39:35 +0100

    Låt det uppdateras

--- test.py
+++ test.py
@@ -5,12 +5,20 @@
 # print("Benereta")
 
 
+# 
 namn = "Bena"
 age = 36
 print(f"Hej,Vad heter du {namn}, hur gammal ar du {age}")
 
-#
+# 
 klockan = 12
 minnuter = 25
 namn = "rolex"
 print(f"vilken klocka anvander du {namn}, vad ar klockan nu {klockan},hur monga minnuter {minnuter}")
+
+
+#
+x = 42
+y = str(x)
+print(y)
+print(type(y))
