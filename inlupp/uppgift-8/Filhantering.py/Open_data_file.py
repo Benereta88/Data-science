2a46659f3969359c5208080dc2ba8f6509a736ce
Benereta88 <beneretahoxha8@gmail.com>
2025-01-08 11:28:28 +0100

    uppdate

--- inlupp/uppgift-8/Filhantering.py/Open_data_file.py
+++ inlupp/uppgift-8/Filhantering.py/Open_data_file.py
@@ -1,5 +1,14 @@
 
-import csv
+# 1. Skapa en file som heter data.text
+#with open("data.text", "w", newline="") as file:
+ #file.write("Product","Price","Stock"),
+ #file.write("Apples",5,100),
+ #file.write("Bananas",3,150),
+ #file.write("Oranges",4,200)
 
-with open("output.csv", "w", newline="") as file:
- writer = csv.writer(file)
+
+
+with open("personner-1.txt", "w") as file:
+ file.write("Name, age, City)/nMaria, 25, Stockholm/n")
+
+
