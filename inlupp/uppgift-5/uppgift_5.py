a1e1fc8ec65698d120fa1103f0b5eb61b948d1a2
Benereta88 <beneretahoxha8@gmail.com>
2025-01-13 20:29:20 +0100

    uppdatera

--- inlupp/uppgift-5/uppgift_5.py
+++ inlupp/uppgift-5/uppgift_5.py
@@ -3,10 +3,8 @@
 
     
 def filter_odd(numbers: list) -> list:
-        """
-        Funktionen returnerar en lista med alla jämna tal från den givna listan.
-        """
-        return [num for num in numbers if num % 2 == 0] 
+        # Funktionen returnerar en lista med alla jämna tal från den givna listan.
+          return [num for num in numbers if num % 2 == 0] 
 
 print(filter_odd([1, 2, 3, 4]) == [2, 4])
 print(filter_odd([1, 3, 5]) == [])
