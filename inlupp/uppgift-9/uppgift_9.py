368eae148940eb6a86561642607bc654a594bc27
Benereta88 <beneretahoxha8@gmail.com>
2025-01-13 20:41:33 +0100

    uppdatera

--- inlupp/uppgift-9/uppgift_9.py
+++ inlupp/uppgift-9/uppgift_9.py
@@ -2,7 +2,7 @@
 # Skapa en funktion is_palindrome(string) som kontrollerar om en given sträng är ett palindrom (dvs. samma framifrån och bakifrån).
 
 
-def is_palindrome(string: str) -> bool: #
+def is_palindrome(string: str) -> bool: 
     return string == string[::-1] # Retunerar True om strängen är samma framifrån och bakifrån, annan False
 
 print(is_palindrome("radar") == True)
