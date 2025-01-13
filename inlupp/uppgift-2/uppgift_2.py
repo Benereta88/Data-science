55adbb9deb402f7d791a74bb012f06739b67a2a0
Benereta88 <beneretahoxha8@gmail.com>
2025-01-13 11:37:48 +0100

    Ändringar uppgift 2

--- inlupp/uppgift-2/uppgift_2.py
+++ inlupp/uppgift-2/uppgift_2.py
@@ -1,14 +1,15 @@
 # Uppgift 2
 # Skapa en funktion sum_list(numbers) som returnerar summan av alla siffror i listan.
 
-
 def sum_list(numbers: list[int]) -> int: # Funktionen tar en parameter nummers av datatypen list[int] och retunerar en int.
     return sum(numbers) # Retunerar summan av alla siffror i listan.
 
 print (sum_list([1, 2, 3, 4, 5, 6]))
 print(sum_list([1, 2, 3]) == 6)
 print(sum_list([]) == 0)
-print(sum_list([-1, -1, 2]) ==21) 
+print(sum_list([-1, -1, 2]) == 0)
+print(sum_list([1, 2, 3, 4, 5, 6]) == 21)
+
 
 
 
